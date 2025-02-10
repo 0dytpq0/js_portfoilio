ab92e37b1c1cb1386510c49ffd95d99aa62d0263
0dytpq0 <rkfnahs12e@gmail.com>
2025-02-10 14:02:46 +0900

    feat : 프로젝트 버튼 인터렉션 추가

--- src/components/organisms/ProjectsSection.tsx
+++ src/components/organisms/ProjectsSection.tsx
@@ -1,19 +1,55 @@
 "use client";
 
+import { useState } from "react";
 import { projects } from "@/constants/data/projects";
 import ProjectItem from "../molecules/ProjectItem";
-import ProjectsModal from "./ProjectsModal";
 import SectionWrapper from "./SectionWrapper";
+import { TiChevronRight, TiChevronLeft } from "react-icons/ti";
 
 function ProjectsSection() {
+  const projectKeys = Object.keys(projects);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevProject = () => {
+    setCurrentIndex((prev) => (prev === 0 ? projectKeys.length - 1 : prev - 1));
+  };
+
+  const nextProject = () => {
+    setCurrentIndex((prev) => (prev === projectKeys.length - 1 ? 0 : prev + 1));
+  };
+
   return (
-    <>
-      <SectionWrapper title="Projects">
-        {Object.keys(projects).map((key) => (
-          <ProjectItem key={key} name={key} />
-        ))}
-      </SectionWrapper>
-    </>
+    <SectionWrapper title="Projects">
+      <div className="w-full relative flex flex-grow items-center">
+        {/* 왼쪽 화살표 */}
+        <TiChevronLeft
+          onClick={prevProject}
+          className="
+            absolute left-[-50px] top-1/2 -translate-y-1/2 text-[#a16848] 
+            hover:text-[#7c584a] hover:scale-125 transition-transform
+            bg-white/30 backdrop-blur-md p-2 rounded-full shadow-lg cursor-pointer"
+          size={40}
+        />
+
+        {/* 현재 프로젝트 표시 */}
+        <div className={`w-full flex items-center`}>
+          <ProjectItem
+            key={projectKeys[currentIndex]}
+            name={projectKeys[currentIndex]}
+          />
+        </div>
+
+        {/* 오른쪽 화살표 */}
+        <TiChevronRight
+          onClick={nextProject}
+          className="
+            absolute right-4 top-1/2 -translate-y-1/2 text-[#a16848] 
+            hover:text-[#7c584a] hover:scale-125 transition-transform
+            bg-white/30 backdrop-blur-md p-2 rounded-full shadow-lg cursor-pointer"
+          size={40}
+        />
+      </div>
+    </SectionWrapper>
   );
 }
 
